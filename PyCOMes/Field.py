73171d917ed68764b62b7af35b22065c4c16fb35
ft1019 <ft1019@vm-ltsp-deb9.physik.privat>
2019-03-06 00:41:44 +0100

    PyCOMes first step.

---
+++ PyCOMes/Field.py
@@ -0,0 +1,108 @@
+from .utils import *
+
+class FieldNotFound(Exception):
+    pass
+
+class InvalidArgument(ValueError):
+    pass
+
+class Field():
+    
+    def __init__(self, file_name=None, skip_rows=9):
+        
+        self.file_name=file_name
+        self._skip_rows=skip_rows
+        
+        if file_name==None:
+            self.field=pd.DataFrame({})
+            self.params={}
+            self.vars={}
+            self.dimension=0
+            return
+            
+        if not isinstance(file_name, str):
+            raise InvalidArgument
+        
+        self.params=read_params(file_name)
+        self.vars=read_vars(file_name)
+        self.dimension=read_dimension(file_name)
+        self._load_txt()
+        return
+    
+    def _load_txt(self):
+        
+        self.head=make_head(self.file_name)
+        self.field=pd.read_csv(self.file_name, skiprows=self._skip_rows, delim_whitespace=True, header=None,
+                               names=self.head, usecols=self.head)
+        
+        if self.dimension==2:
+            try:
+                self.X=np.unique(self.field.x)
+                self.Y=np.unique(self.field.y)
+            except:
+                self.X=np.unique(self.field.r)
+                self.Y=np.unique(self.field.z)
+            
+        if self.dimension==3:
+            self.X=np.unique(self.field.x)
+            self.Y=np.unique(self.field.y)
+            self.Z=np.unique(self.field.z)
+        
+        return
+    
+    def load_txt(self,file_name,skip_rows=9,verbose=False):
+        
+        self.file_name=file_name
+        self._skip_rows=skip_rows
+        self._load_txt(file_name) 
+        if verbose:
+            self.file_header()
+        return
+    
+    def file_header(self):
+        
+        with open(self.file_name,'r') as f:
+            for i in range(self._skip_rows):
+                print(f.readline())
+        return
+
+    def set_parameters(self, params: dict):
+        
+        if len(params)!=len(self.params):
+            raise InvalidArgument("the number of parameters selected does not match the number of parameters needed.")
+        if not all(p in self.params.keys() for p in params):
+            raise InvalidArgument(f"unknow parameters. The accepted parameters are: {list(self.params.keys())}.")
+        if not self.params:
+            return
+        self.selected_params=params
+        self._selected_params_suffix=[]
+        for i in self.params.keys():
+            if not params[i] in self.params[i] and not params[i] in [str(p) for p in self.params[i]]:
+                raise InvalidArgument(f"value not accepted for {i} parameter. The accepted values are {list(self.params[i])}.")
+            self._selected_params_suffix.append(f'{i}={params[i]}')
+        return
+    
+    def _contains_field(self):
+        
+        if self.dimension==2:
+            return all(i in self.vars.keys() for i in ['Ex','Ey']) or all(i in self.vars.keys() for i in ['Er','Ez'])
+        if self.dimension==2:
+            return all(i in self.vars.keys() for i in ['Ex','Ey','Ez'])
+    
+    def get_field_components(self):
+        
+        if not self._contains_field():
+            raise FieldNotFound
+        e_field_components=['Ex', 'Ey'] if 'Ex' in self.vars.keys() else ['Er', 'Ez']
+        string_Ex=[i for i in self.head if all(k in i for k in [e_field_components[0]]+self._selected_params_suffix)][0]
+        Ex=self.field[string_Ex]
+        string_Ey=[i for i in self.head if all(k in i for k in [e_field_components[1]]+self._selected_params_suffix)][0]
+        Ey=self.field[string_Ey]
+        if self.dimension==3:
+            e_field_components.append('Ez')
+            string_Ez=[i for i in self.head if
+                       all(k in i for k in [e_field_components[2]]+self._selected_params_suffix)][0]
+            Ez=self.field[strin_Ez]
+            return Ex, Ey, Ez
+        return Ex, Ey
+        
---
+++ PyCOMes/utils.py
@@ -0,0 +1,127 @@
+import numpy as np
+from numba import jit, jitclass
+import pandas as pd
+
+def num(s):
+    try:
+        k=int(s)
+    except:
+        k=float(s)
+    return k
+
+class WrongDimension(Exception):
+    pass
+
+def read_params(file, skip_rows=8):
+
+    """
+        Returning the sweep parameters for a COMSOL simulation exported in a text file.
+        
+        - file: name of the text file;
+        - skip_rows: rows to skip with data regarding the simulation.
+    """
+        
+    with open(file,'r') as f:
+        for i in range(skip_rows):
+            f.readline()
+        head=f.readline()
+        f.close()
+    head=np.unique(head.split(' '))
+    head=[h.replace(',','') for h in head if '=' in h]
+    if not len(head):
+        return None
+    head_field=[]
+    head_value={}
+    for h in head:
+        h.replace('/n', '')
+        field,value=h.split('=')
+        head_field.append(field)
+        if field not in head_value:
+            head_value[field]=[num(value)]
+        else:
+            if num(value) in head_value[field]:
+                continue
+            head_value[field].append(num(value))
+    return head_value
+
+def make_head(file,skip_rows=8):
+    
+    """
+    Returning the head to use when loading the field from the COMSOL output text file.
+    
+    - file: name of the text file;
+    - skip_rows: rows to skip with data regarding the simulation.
+    """
+
+    with open(file,'r') as f:
+        for i in range(skip_rows):
+            line=f.readline()
+            if 'Dimension' in line:
+                dim=int(line.split()[-1])
+        head=f.readline()
+        f.close()
+    fields=read_params(file,skip_rows).keys()
+    var=[v for v in read_vars(file,skip_rows).keys()]
+    head=head.split(' ')
+    head=np.array([h.replace(',','') for h in head if '=' in h])
+    head=np.reshape(head, (len(head)//len(fields), len(fields)))
+    skip=len(var)-dim if var else dim
+    head=head[::skip]
+    HEAD=var[:dim]
+    for h in head:
+        for e in var[dim:]:
+            HEAD.append(e+' '+' '.join(h).replace('\n',''))
+    return HEAD
+
+def read_vars(file,skip_rows=8):
+    
+    """
+    Returning the variables stored in the input txt file from COMSOL (e.g. x, y, z, normE, etc.).
+    
+    - file: name of the text file;
+    - skip_rows: rows to skip with data regarding the simulation.
+    """
+    
+    variables={}
+    with open(file,'r') as f:
+        for i in range(skip_rows):
+            line=f.readline()
+            if 'Length unit' in line:
+                len_unit=line.split()[-1]
+            if 'Dimension' in line:
+                dim=int(line.split()[-1])
+        head=f.readline()
+        f.close()
+    head=head.split()
+    head=[h for h in head if h not in ['%','@'] and '=' not in h and ':' not in h]
+    coordinates=head[:dim]
+    for c in coordinates:
+        variables[c]=len_unit
+    for i in range(0,len(head[dim:]),2):
+        if '.' not in head[dim:][i]:
+            continue
+        v=head[dim:][i].split('.')[-1]
+        if v in variables:
+            break
+        else:
+            unit=head[dim:][i+1].replace(')','')
+            unit=unit.replace('(','')
+            variables[v]=unit
+    return variables
+
+def read_dimension(file):
+    
+    """
+    Returning the dimension of the COMSOL simulation.
+    
+    - file: name of the text file.
+    """
+    
+    with open(file,'r') as f:
+        while True:
+            line=f.readline()
+            if 'Dimension' in line:
+                dim=int(line.split()[-1])
+                f.close()
+                break
+    return dim
